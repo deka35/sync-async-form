3bcfe8125ba564dd2b8359461b48618493c9a05f
Alumno09 J601 <a09j601@campustls.local>
2017-10-31 12:53:38 -0500

    Add

---
+++ public/main.js
@@ -0,0 +1,11 @@
+$(document).ready (function (){
+	
+	$('input[type="submit"]').on("click", function(){
+
+	var firs_name = $('input[name="first_name"]').val();
+	console.log(first_name);
+
+	event.preventDefault();
+});
+
+})
