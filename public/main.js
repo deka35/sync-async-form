292ed53b5486fae954a5287cd6d2b98419a97dba
Alumno09 J601 <a09j601@campustls.local>
2017-11-07 12:43:23 -0500

    agrego 07/11/17

--- public/main.js
+++ public/main.js
@@ -1,11 +1,31 @@
 $(document).ready (function (){
+	alert("Hola");
 	
-	$('input[type="submit"]').on("click", function(){
+	$('input[type="submit"]').on("click", function(event){
 
-	var firs_name = $('input[name="first_name"]').val();
+	/*var firs_name = $('input[name="first_name"]').val();
 	console.log(first_name);
 
+	event.preventDefault();*/
+
 	event.preventDefault();
+
+	var First_name = $('#First_name').val();
+	
+	$.post('find.html', {'First_name':First_name});
+	$.get('find.html', {'First_name':First_name});
+
+	/*$.ajax('find.html', {
+		method: 'post',
+		data: {
+			'First_name': First_name
+		}
+
+	})
+	$post()console.log('Hiciste click' , $('#First_name').val());
+	console.log('Hiciste click' , $('#last_name').val());
+	console.log('Hiciste click' , $('input[name="gender"]:checked').val());
+	console.log('Hiciste click' , $('input[name="interest"]:checked').val());*/
 });
 
-})
+});
